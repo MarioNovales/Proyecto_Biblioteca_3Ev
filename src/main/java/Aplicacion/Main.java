fe0c1d455a3be70a13c7c42d4a6c0efaf629b5f0
Mario Novales <220004@fppiramide.com>
2023-04-27 10:17:17 +0200

    mejorando

--- src/main/java/Aplicacion/Main.java
+++ src/main/java/Aplicacion/Main.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new VentaGestora();
 
+        });
     }
 
 }
---
+++ src/main/java/Aplicacion/VentaGestora.java
@@ -0,0 +1,16 @@
+package Aplicacion;
+
+import javax.swing.*;
+
+public class VentaGestora extends JFrame {
+
+    public VentaGestora(){
+
+        super("Gesion de la biblioteca");
+        setSize(600,400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+}
