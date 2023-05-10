3c882c1de9ab6990c265a086b1ba682d3b8ff1c8
MarioNovales <220004@fppiramide.com>
2023-05-10 17:42:37 +0200

    Empezadas las ventanas y terminado conexion

---
+++ src/main/java/XML/Lectura.java
@@ -0,0 +1,16 @@
+package XML;
+
+import Aplicacion.Libro;
+
+import java.util.ArrayList;
+
+public class Lectura {
+
+    public static ArrayList<Libro> lecturaXml(){
+
+        ArrayList<Libro> listaLibros = new ArrayList<>();
+
+
+        return listaLibros;
+    }
+}
